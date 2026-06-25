7c694a8217d4e0c19c1ee5b4ba5fcbe675de5d5a
Sayan Hazra <sayanhazra512@gmail.com>
2026-06-25 12:21:00 +0530

    Implement star animation on canvas

--- themes/script.js
+++ themes/script.js
@@ -1,3 +1,47 @@
 ```javascript
-console.log("Portfolio Loaded Successfully");
+const canvas = document.getElementById("stars");
+const ctx = canvas.getContext("2d");
+
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+
+let stars = [];
+
+for (let i = 0; i < 150; i++) {
+    stars.push({
+        x: Math.random() * canvas.width,
+        y: Math.random() * canvas.height,
+        r: Math.random() * 2
+    });
+}
+
+function drawStars() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    ctx.fillStyle = "white";
+
+    for (let star of stars) {
+        ctx.beginPath();
+        ctx.arc(star.x, star.y, star.r, 0, Math.PI * 2);
+        ctx.fill();
+    }
+
+    moveStars();
+}
+
+function moveStars() {
+    for (let star of stars) {
+        star.y += 0.2;
+        if (star.y > canvas.height) {
+            star.y = 0;
+            star.x = Math.random() * canvas.width;
+        }
+    }
+}
+
+function animate() {
+    drawStars();
+    requestAnimationFrame(animate);
+}
+
+animate();
 ```
